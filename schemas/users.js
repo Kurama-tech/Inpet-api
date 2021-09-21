f184b423b06307b80a7a554ae94c7810f4bda41a
Meer Sawood <msawood@redhat.com>
2021-09-21 21:09:59 +0530

    chore: added exception for connection

--- schemas/users.js
+++ schemas/users.js
@@ -3,7 +3,18 @@ const moongoose = require('mongoose');
 const {supplierSchema} = require('./supplierSchema');
 const {customerSchema} = require('./customerSchema');
 const DB_URL = process.env.MONGO_DB_URL;
-moongoose.connect(DB_URL);
+
+async function connectDB(){
+try {
+    await moongoose.connect(DB_URL);
+    console.log('Connected Successfully');
+    moongoose.connection.useDb('inpet');
+ } catch (error) {
+    console.log('Error connecting to DB ::', error);
+}
+}
+
+connectDB();
 
 var db = moongoose.connection;
 
