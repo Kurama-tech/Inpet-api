fc91ed90a038882d16c406a669a9f89a2d4483f1
Meer Sawood <msawood@redhat.com>
2021-09-21 22:08:14 +0530

    fix : db issue

--- schemas/users.js
+++ schemas/users.js
@@ -3,12 +3,13 @@ const moongoose = require('mongoose');
 const {supplierSchema} = require('./supplierSchema');
 const {customerSchema} = require('./customerSchema');
 const DB_URL = process.env.MONGO_DB_URL;
+const DB_NAME = process.env.MONGO_DB_NAME;
 
 async function connectDB(){
 try {
-    await moongoose.connect(DB_URL+'inpet');
+    await moongoose.connect(DB_URL+DB_NAME);
     console.log('Connected Successfully');
-    return moongoose.connection.useDb('inpet');
+    return moongoose.connection;
  } catch (error) {
     console.log('Error connecting to DB ::', error);
 }
