8a4aa2a3bf22179d63bf7e9e9e6995d827caed11
Meer Sawood <msawood@redhat.com>
2021-09-21 23:12:09 +0530

    added another db connect fix

--- schemas/users.js
+++ schemas/users.js
@@ -2,23 +2,27 @@ const dotenv = require('dotenv').config();
 const moongoose = require('mongoose');
 const {supplierSchema} = require('./supplierSchema');
 const {customerSchema} = require('./customerSchema');
-const { options } = require('../routes/api');
 const DB_URL = process.env.MONGO_DB_URL;
 const DB_NAME = process.env.MONGO_DB_NAME;
 
 var options = {};
-if(process.env.MONGO_USERNAME !== 'undefined' && process.env.MONGO_PASSWORD !== 'undefined' ){
+if(process.env.MONGO_USERNAME && process.env.MONGO_PASSWORD){
     console.log('It is set! running in prod mode!');
+    console.log(process.env.MONGO_USERNAME);
     options = {
+        dbName: DB_NAME,
         user: process.env.MONGO_USERNAME,
         pass: process.env.MONGO_PASSWORD
     }
 } else {
+    options = {
+        dbName: DB_NAME 
+    }
     console.log('Running in Dev mode');
 }
 async function connectDB(){
 try {
-    await moongoose.connect(DB_URL+DB_NAME, options);
+    await moongoose.connect(DB_URL, options);
     console.log('Connected Successfully');
     return moongoose.connection;
  } catch (error) {
