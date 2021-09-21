50e38014a58375b6c2c8134927b33ec64aff9d53
Meer Sawood <msawood@redhat.com>
2021-09-21 22:23:42 +0530

    fix

--- schemas/users.js
+++ schemas/users.js
@@ -2,12 +2,23 @@ const dotenv = require('dotenv').config();
 const moongoose = require('mongoose');
 const {supplierSchema} = require('./supplierSchema');
 const {customerSchema} = require('./customerSchema');
+const { options } = require('../routes/api');
 const DB_URL = process.env.MONGO_DB_URL;
 const DB_NAME = process.env.MONGO_DB_NAME;
 
+var options = {};
+if(process.env.MONGO_USERNAME !== 'undefined' && process.env.MONGO_PASSWORD !== 'undefined' ){
+    console.log('It is set! running in prod mode!');
+    options = {
+        user: process.env.MONGO_USERNAME,
+        pass: process.env.MONGO_PASSWORD
+    }
+} else {
+    console.log('Running in Dev mode');
+}
 async function connectDB(){
 try {
-    await moongoose.connect(DB_URL+DB_NAME);
+    await moongoose.connect(DB_URL+DB_NAME, options);
     console.log('Connected Successfully');
     return moongoose.connection;
  } catch (error) {
